288b1ec10b9f48c54daf02e595dbd58a2de23ee4
Mike Crowe <drmikecrowe@gmail.com>
2019-06-22 07:03:38 -0400

    Cleanup up unneeded files

--- my_crawler/run.py
+++
@@ -1,6 +0,0 @@
-import re
-import sys
-
-from scrapy.cmdline import execute
-
-execute(sys.argv)
--- news_crawler/run.py
+++
@@ -1,2 +0,0 @@
-from scrapy.cmdline import execute
-execute(['scrapy','runspider', '/home/mcrowe/Programming/Personal/show_media_bias/news_crawler/crawler/spiders/blogspider.py'])
